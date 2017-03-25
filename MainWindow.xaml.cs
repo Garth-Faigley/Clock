3a7586bbc33d7553f4edad36951ddac2f5fc0c1a
GF1 <GF1@GF1612>
2017-03-25 07:01:52 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Time _clock;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _clock = new Time();
+            var timer = new DispatcherTimer();
+            DataContext = _clock;
+
+            timer.Tick += TimerTick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+        }
+
+
+        //timer event handler
+        private void TimerTick(object sender, EventArgs e)
+        {
+            // get current time
+            var hour = DateTime.Now.Hour;
+            var minute = DateTime.Now.Minute;
+            var second = DateTime.Now.Second;
+            var amPm = "AM";
+
+            //time
+            var time = "";
+
+            //padding leading zero
+            if (hour > 12)
+            {
+                time += (hour - 12);
+                amPm = "PM";
+            }
+            else
+            {
+                time += hour;
+            }
+
+            time += ":";
+
+            if (minute < 10)
+            {
+                time += "0" + minute;
+            }
+            else
+            {
+                time += minute;
+            }
+            time += ":";
+
+            if (second < 10)
+            {
+                time += "0" + second;
+            }
+            else
+            {
+                time += second;
+            }
+
+            time += " " + amPm;
+
+            //update label
+            _clock.CurrentTime = time;
+        }
+
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Clock.Annotations;
+
+namespace Clock
+{
+    public class Time : INotifyPropertyChanged
+    {
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get { return _currentTime; }
+            set { _currentTime = value; OnPropertyChanged("CurrentTime"); }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
